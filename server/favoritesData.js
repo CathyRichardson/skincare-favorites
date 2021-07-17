da40ceda29e66df3cec68326268e2c226cd29118
Cathy Richardson <cathyanne08@gmail.com>
2021-07-17 17:26:42 -0600

    added my data array info

--- server/favoritesData.js
+++ server/favoritesData.js
@@ -7,16 +7,64 @@ const favoritesArr = [
     },
     {
         "id": 1,
-        "type": "First Cleanse/ Makeup Remover",
-        "name": "DHC Deep Cleansing Oil",
-        "picture": "https://www.dhccare.com/media/catalog/product/cache/02cccf94ae90183c4f9b773c23a87fc4/d/h/dhc_deep_cleansing_oil_h_300.png"
+        "type": "2nd Cleanse/ AM Face Wash",
+        "name": "Vanicream Gentle Facial Cleanser",
+        "picture": "https://www.vanicream.com/dynamic-media/product/images/vanicream-facial-cleanser-8oz-front-bp20a.jpg?gravity=center&v=galleryZoom&k=6kRgk91BEufhjMU%2BV6bf6A"
     },
     {
         "id": 2,
-        "type": "First Cleanse/ Makeup Remover",
-        "name": "DHC Deep Cleansing Oil",
-        "picture": "https://www.dhccare.com/media/catalog/product/cache/02cccf94ae90183c4f9b773c23a87fc4/d/h/dhc_deep_cleansing_oil_h_300.png"
-    }
+        "type": "Vitamin C Serum",
+        "name": "DERMA E Vitamin C Serum",
+        "picture": "https://cdn.shopify.com/s/files/1/1525/1400/products/0365_front_540x.jpg?v=1569333261"
+    },
+    {
+        "id": 3,
+        "type": "Hydrating Serum",
+        "name": "Vichy Minéral 89 Face Serum",
+        "picture": "https://www.vichyusa.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-acd-vichy-master-catalog/default/dw889a10f7/product/Mineral-89-Hyaluronic-Acid-Moisturizer-50ml-Vichy-PDP-3337875543248.jpg?sw=565&sh=400&sm=fit&q=70"
+    },
+    {
+        "id": 4,
+        "type": "Exfoilator",
+        "name": "Paula's Choice 2% BHA Liquid Exfoliant",
+        "picture": "https://www.paulaschoice.com/dw/image/v2/BBNX_PRD/on/demandware.static/-/Sites-pc-catalog/default/dwf0fd0988/images/products/skin-perfecting-2-percent-bha-liquid-2010-L.png?sw=360&sfrm=png"
+    },
+    {
+        "id": 5,
+        "type": "Retinoid",
+        "name": "Differin Gel",
+        "picture": "https://differin.com/sites/default/files/images/product/differin-gel-asset-1%402x-gel.png"
+    },
+    {
+        "id": 6,
+        "type": "Facial Moisturizer",
+        "name": "CeraVe Moisturizing Cream",
+        "picture": "https://www.cerave.com/-/media/project/loreal/brand-sites/cerave/americas/us/products-v4/moisturizing-cream/cerave_moisturizing_cream_16oz_jar_front-700x875-v3.jpg?rev=9a4aae1dc0f24b42860224d26fccf90f?w=500&hash=E5099501099A14AF10A785F5DA659E5A"
+    },
+    {
+        "id": 7,
+        "type": "Facial Oil",
+        "name": "Acure Seriously Soothing Blue Tansy Night Oil",
+        "picture": "https://target.scene7.com/is/image/Target/GUEST_cecc633a-d524-47c4-8376-373323ac50f6?fmt=webp&wid=1400&qlt=80"
+    },
+    {
+        "id": 8,
+        "type": "Facial SPF",
+        "name": "Colorescience Sunforgettable Total Protection Face Shield",
+        "picture": "https://cdn.shopify.com/s/files/1/0251/2184/9419/products/face-original_720x.png?v=1588194055"
+    },
+    {
+        "id": 9,
+        "type": "Lip Product",
+        "name": "Aquaphor Healing Ointment",
+        "picture": "https://www.aquaphorus.com/wp-content/uploads/2016/06/healing-1_75_ver_2.jpg"
+    },
+    {
+        "id": 10,
+        "type": "Body Moisturizer",
+        "name": "AmLactin Daily Moisturizing Lotion",
+        "picture": "https://target.scene7.com/is/image/Target/GUEST_9e2a5df1-af9c-4fd8-8619-ae3b4dbbcebb?fmt=webp&wid=1400&qlt=80"
+    },
 ]
 module.exports = {
     favoritesArr,
